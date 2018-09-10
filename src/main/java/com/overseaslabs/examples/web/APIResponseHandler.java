b46dda939e25faf45577309db5cec49473ebbb46
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-09-10 11:18:00 +0300

    don't throw on 4XX API responses

---
+++ src/main/java/com/overseaslabs/examples/web/APIResponseHandler.java
@@ -0,0 +1,26 @@
+package com.overseaslabs.examples.web;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+@Component
+public class APIResponseHandler implements ResponseErrorHandler {
+    private static final Logger log = LoggerFactory.getLogger(APIResponseHandler.class);
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        log.error("Response error: {} {}", response.getStatusCode(), response.getStatusText());
+    }
+
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        return response.getStatusCode().series() == HttpStatus.Series.SERVER_ERROR;
+    }
+}
