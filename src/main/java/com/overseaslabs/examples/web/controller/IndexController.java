01ae48eae707d38fe4918efc4a582170ba646d74
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-09-11 11:05:28 +0300

    add mapping for the info route

--- src/main/java/com/overseaslabs/examples/web/controller/IndexController.java
+++ src/main/java/com/overseaslabs/examples/web/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping({"/", "/users", "/emails"})
+    @GetMapping({"/", "/users", "/emails", "/info"})
     public String index() {
         return "index";
     }
